90fc8569dfdfc0105ea66fa27651106eabffe624
kvartira476 <obrabot@live.com>
2022-11-30 22:07:51 +0300

    commit 4 home work

--- 3Lesson/3_4.py
+++ 3Lesson/3_4.py
@@ -1,4 +1,4 @@
-z = float(input('Number1:'))
+z = float(input('Number1:'))        #float ,так как думаю int не подходит, если ввести дробное число
 x = float(input('Number2:'))
 c = float(input('Number3:'))
 q = (z > 0) + (x > 0) + (c > 0)     #True,False
