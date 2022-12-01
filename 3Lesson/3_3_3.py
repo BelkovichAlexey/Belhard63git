1b9382a822d742c277f7994cb992bfaf09398832
kvartira476 <obrabot@live.com>
2022-12-01 21:41:39 +0300

    format 3 commit

--- 3Lesson/3_3_3.py
+++ 3Lesson/3_3_3.py
@@ -1,5 +1,5 @@
 z = input('Name:')
-x = int(input('Age:'))   # int пришлось вставить, %d воспринимал как строку
+x = int(input('Age:'))   # int пришлось вставить, %d воспринимал как строку а это число, ошибку писал
 c = input('City:')
 q = 'My name is %s. I am %d years old. I am from %s, Thanks!' % (z, x, c)
 print(q)
