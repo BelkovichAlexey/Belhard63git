fccf0f9a753d12acfbb4ea76b90cf191a6e49df0
kvartira476 <obrabot@live.com>
2022-12-16 18:02:50 +0300

    Binary Search

---
+++ 7Lesson/7_2.py
@@ -0,0 +1,17 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+def binary_search(list, element):
+    list.sort()
+    start = 0
+    end = len(list)
+    while start <= end:
+        centr = (start + end) // 2
+        if element == centr:
+            return list.index(element)
+        else:
+            if element > centr:
+                start = centr +1
+            else:
+                end = centr -1
+    return "no element"
+
+print(binary_search(list, 11))
