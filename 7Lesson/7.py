bf9af0a15af0e3c1fd4ca3d859394a95f7645a06
kvartira476 <obrabot@live.com>
2022-12-20 18:06:40 +0300

    Class

--- 7Lesson/7.py
+++ 7Lesson/7.py
@@ -19,17 +19,23 @@ class Button:
         self.is_pressed = False
 
     @classmethod
+
     def change_color(cls, new_color):
         if not isinstance(new_color, str):
             raise TypeError
         cls.color = new_color
 
-    def press(self):
-        self.is_pressed = True
+    # def press(self):
+    #     self.is_pressed = True
 
-    # def __str__(self):
-    #     return f'{self.text}'
+    def __str__(self):
+        return f'{self.text}'
 
+    # def to_dict(self):
+    #     {self.widht: widht,
+    #      self.heiht: heiht,
+    #      self.text: text
+    #     }
 
 enter = Button(widht=12, height=5, text='enter')
 Button.change_color('red')
@@ -37,7 +43,7 @@ print(Button.color)
 
 print(enter.press)
 print(enter.text)
-
+print(enter.to_dict)
 
 
 
