f824cfcc950a7a2c0c4b67e7f44fc4730a7d77e0
kvartira476 <obrabot@live.com>
2022-12-26 20:55:11 +0300

    Class_method

---
+++ 9Lesson/9.py
@@ -0,0 +1,48 @@
+class Car(object):
+
+    def __init__(self, color:str, count_passenger_seat:int, is_baby_seat:bool, is_busy:bool):
+
+        self.is_busy = False
+        self.is_baby_seat = is_baby_seat
+        self.color = color
+        self.count_passenger_seat = count_passenger_seat
+
+    def __str__(self)-> str:
+        return f'Car {self.count_passenger_seat=} {self.is_baby_seat=} {self.is_busy=} {self.color=}'
+
+
+class Taxi(object):
+
+    def __init__(self, cars: list[Car]):
+        self.cars = cars
+
+    def find_car(self, count_passenger: int, is_baby: bool) -> Car | None:
+        if is_baby:
+            cars = list(filter(lambda x: not x.is_busy and x.is_baby_seat, self.cars))
+        else:
+            cars = list(filter(lambda x: not x.is_busy, self.cars))
+        for car in cars:
+            if car.count_passenger_seat >= count_passenger:
+                car.is_busy = True
+                return car
+
+
+class Category(object):
+    categories = []
+    # name = None
+
+    @classmethod
+    def add(cls, categories:str, name:str) -> int:
+        cls.categories = categories
+        for name in cls.categories:
+            if name not in cls.categories:
+                cls.categories = cls.categories.append(name)
+            else:
+                raise ValueError ('данная категория уже существует')
+            return cls.categories.index(name)
+
+
+
+
+
+
